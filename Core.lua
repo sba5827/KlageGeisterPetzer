f76f447f3f5e3e72d75204649f0d7791c3e346e4
Steven Baldauf <38493688+sba5827@users.noreply.github.com>
2021-04-22 01:20:27 +0200

    Add files via upload

---
+++ Core.lua
@@ -0,0 +1,38 @@
+MY__FRAME = CreateFrame("FRAME");
+
+MY__FRAME:SetScript("OnEvent", function (self, event, ...)
+
+	if (event == "UNIT_SPELLCAST_SUCCEEDED")and arg2 == "Wehklagen der Hochgeborenen" then
+
+		local mRank = nil;
+		local playerName = UnitName("player")
+
+		for i = 1, MAX_RAID_MEMBERS do
+			local name, rank = GetRaidRosterInfo(i)
+			if name == playerName then
+				mRank = rank;
+			end
+		end
+		
+		local name = GetUnitName(arg1)
+		
+		if name == StriLi_Bastard then return end
+		
+		Bastard = name;
+		
+		if(GetNumRaidMembers() > 0) then
+			if mRank >= 1 then
+				SendChatMessage(name .. " hat Klagegeister gestellt, du Bastard!!!", "RAID_WARNING");
+			else 
+				SendChatMessage(name .. " hat Klagegeister gestellt, du Bastard!!!", "RAID");
+			end
+		elseif(GetNumGroupMembers() > 0) then
+			SendChatMessage(name .. " hat Klagegeister gestellt, du Bastard!!!", "PARTY");
+		else
+			SendChatMessage(name .. " hat Klagegeister gestellt, du Bastard!!!", "SAY");
+		end
+	end
+
+end);
+
+MY__FRAME:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED");
